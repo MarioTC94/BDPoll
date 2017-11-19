99d5b3f07508e4deb375a7ea0dd6277b74380406
MarioTC94 <mariotc09@hotmail.com>
2017-11-19 12:54:33 -0600

    dfg

--- View/Forms/AgregarCuestionario.cs
+++ View/Forms/AgregarCuestionario.cs
@@ -12,8 +12,11 @@ namespace View.Forms {
     public partial class AgregarCuestionario : MetroFramework.Forms.MetroForm {
 
         public List<Model.Pregunta> ListPreguntas { get; set; }
+        public List<Helpers.PanelAgregarPregunta> ListPanelAgregar { get; set; }
 
         public AgregarCuestionario(List<Model.Pregunta> ListPreguntas = null) {
+            this.ListPreguntas =  new List<Model.Pregunta>();
+            this.ListPanelAgregar = new List<Helpers.PanelAgregarPregunta>();
             InitializeComponent();
         }
 
@@ -29,6 +32,9 @@ namespace View.Forms {
                 oPregunta.Opciones.Add(new Model.Opciones() { DescripcionOpcion = oForm.Opcion4 });
                 oPregunta.IDTipoPregunta = 2; 
                 this.ListPreguntas.Add(oPregunta);
+                var p = new Helpers.PanelAgregarPregunta(this.ListPreguntas);
+                this.ListPanelAgregar.Add(p);
+                this.pnAgregarCuestionario.Controls.Add(p.getPanel("Respuesta Unica", oForm.DescripcionPregunta));
             }
         }
 
