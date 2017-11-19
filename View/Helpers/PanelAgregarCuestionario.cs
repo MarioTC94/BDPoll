4b27403c8ab0facc080ce07217bf49d5e753396a
Hersey Aguilar <lhersey_db@outlook.com>
2017-11-19 11:37:17 -0600

    Pos me mato

---
+++ View/Helpers/PanelAgregarCuestionario.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroFramework;
+using MetroFramework.Controls;
+using MetroFramework.Forms;
+
+namespace View.Helpers {
+    public class PanelAgregarCuestionario {
+
+        private MetroPanel oPanel;
+        private MetroPanel oBar;
+        private MetroButton btnRemove;
+        private MetroLabel lbTitulo;
+        private MetroLabel lbCantPreguntas;
+        private MetroLabel lbTotal;
+        private List<Model.Cuestionario> ListCuestionario;
+        //-----------------------------------
+        private int IDCuestionario;
+
+        public PanelAgregarCuestionario(ref List<Model.Cuestionario> ListCuestionario, int IDCuestionario) {
+            this.IDCuestionario = IDCuestionario;
+
+            this.lbTitulo = new MetroLabel();
+            this.lbTitulo.Text = "Titulo: ";
+            this.lbTitulo.Size = this.lbTitulo.MinimumSize;
+            this.lbTitulo.Location = new System.Drawing.Point(16, 15);
+
+            this.lbCantPreguntas = new MetroLabel();
+            this.lbCantPreguntas.Text = "Cantidad de preguntas: ";
+            this.lbCantPreguntas.Size = this.lbCantPreguntas.MinimumSize;
+            this.lbCantPreguntas.Location = new System.Drawing.Point(16, 46);
+
+            this.lbTotal = new MetroLabel();
+            this.lbTotal.Text = "Total: $";
+            this.lbTotal.Size = this.lbTotal.MinimumSize;
+            this.lbTotal.Location = new System.Drawing.Point(16, 82);
+
+            this.oPanel = new MetroPanel();
+            this.oPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.oPanel.Height = 113;
+
+            this.oBar = new MetroPanel();
+            this.oBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.oBar.Height = 5;
+            this.oBar.BackColor = MetroColors.Blue;
+
+            this.btnRemove = new MetroButton();
+            this.btnRemove.Text = "Remover";
+            this.btnRemove.Size = new System.Drawing.Size(94, 32);
+            this.btnRemove.Location = new System.Drawing.Point(533, 46);
+            this.btnRemove.Click += (x, y) => {
+                this.ListCuestionario.Remove()
+                this.ListCuestionario.Select(x => x.IDCuestionario = IDCuestionario);
+                this.oPanel.Parent.Controls.Remove(this.oPanel);
+            };
+
+            this.oPanel.Controls.Add(btnRemove);
+            this.oPanel.Controls.Add(lbTitulo);
+            this.oPanel.Controls.Add(lbCantPreguntas);
+            this.oPanel.Controls.Add(lbTotal);
+        }
+       
+    }
+}
---
+++ View/Helpers/PanelAgregarPregunta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroFramework;
+using MetroFramework.Controls;
+using MetroFramework.Forms;
+
+namespace View.Helpers {
+
+    class PanelAgregarPregunta {
+
+        private MetroPanel oPanel;
+        private MetroPanel oBar;
+        private MetroButton btnRemove;
+        private MetroLabel lbPregunta;
+        private MetroLabel lbTipoPregunta;
+        private List<Model.Pregunta> ListPreguntas;
+
+        public PanelAgregarPregunta(ref List<Model.Pregunta> ListPreguntas) {
+            this.ListPreguntas = ListPreguntas;
+
+            this.lbPregunta = new MetroLabel();
+            this.lbPregunta.Location = new System.Drawing.Point(14, 18);
+
+            this.lbTipoPregunta = new MetroLabel();
+            this.lbTipoPregunta.Location = new System.Drawing.Point(14, 65);
+
+            this.btnRemove = new MetroButton();
+            this.btnRemove.Text = "Remover";
+            this.btnRemove.Size = new System.Drawing.Size(94, 32);
+            this.btnRemove.Location = new System.Drawing.Point(305, 37);
+            this.btnRemove.Click += (e, x) => {
+                this.ListPreguntas.Remove(this.ListPreguntas.FirstOrDefault(x => x.))
+            };
+
+            this.oPanel = new MetroPanel();
+            this.oPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.oPanel.Height = 100;
+        }
+    }
+}
