5090b827bac519b2cb4c42c10e6edd7bf5a8018d
MarioTC94 <mariotc09@hotmail.com>
2017-11-19 09:54:07 -0600

    Se añadió varias clases en Business Logic en cuestión de Cuestionario

--- BusinessLogic/Cuestionario.cs
+++ BusinessLogic/Cuestionario.cs
@@ -16,9 +16,13 @@ namespace BusinessLogic {
                 if (String.IsNullOrEmpty(oCuestionario.Descripcion)) throw new ArgumentNullException("Descripcion", "Por favor ingrese la descripción del cuestionario");
 
                 foreach (Model.Pregunta oPregunta in oCuestionario.Pregunta) {
+                    if (String.IsNullOrEmpty(oPregunta.DescripcionPregunta)) throw new ArgumentNullException("DescripcionPregunta", "La pregunta no puede estar vacía");
+
+                    foreach (Model.Opciones oOpcion in oPregunta.Opciones) {
+                        if (String.IsNullOrEmpty(oOpcion.DescripcionOpcion)) throw new ArgumentNullException("DescripcionPregunta", "Escriba una opción correcta");
+                    }
                     
-                }
-                
+                }              
 
                 return new Data.clsCuestionario().InsertarCuestionario(oCuestionario);
 
