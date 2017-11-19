7b1ff255ec6695d4701de8f2e7f439fea32f710d
MarioTC94 <mariotc09@hotmail.com>
2017-11-19 12:22:59 -0600

    Añadidos diferentes clases

--- View/Forms/Contestar Cuestionario.Designer.cs
+++ View/Forms/Contestar Cuestionario.Designer.cs
@@ -25,18 +25,10 @@
         private void InitializeComponent() {
             this.pnContestarCuestionario = new MetroFramework.Controls.MetroPanel();
             this.btnEnviarCuestionario = new MetroFramework.Controls.MetroButton();
-            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
-            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
-            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
-            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
-            this.metroButton1 = new MetroFramework.Controls.MetroButton();
-            this.pnContestarCuestionario.SuspendLayout();
-            this.metroPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnContestarCuestionario
             // 
-            this.pnContestarCuestionario.Controls.Add(this.metroPanel1);
             this.pnContestarCuestionario.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnContestarCuestionario.HorizontalScrollbarBarColor = true;
             this.pnContestarCuestionario.HorizontalScrollbarHighlightOnWheel = false;
@@ -58,60 +50,6 @@
             this.btnEnviarCuestionario.Text = "Enviar Cuestionario";
             this.btnEnviarCuestionario.UseSelectable = true;
             // 
-            // metroPanel1
-            // 
-            this.metroPanel1.Controls.Add(this.metroButton1);
-            this.metroPanel1.Controls.Add(this.metroLabel3);
-            this.metroPanel1.Controls.Add(this.metroLabel2);
-            this.metroPanel1.Controls.Add(this.metroLabel1);
-            this.metroPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.metroPanel1.HorizontalScrollbarBarColor = true;
-            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
-            this.metroPanel1.HorizontalScrollbarSize = 10;
-            this.metroPanel1.Location = new System.Drawing.Point(0, 0);
-            this.metroPanel1.Name = "metroPanel1";
-            this.metroPanel1.Size = new System.Drawing.Size(672, 113);
-            this.metroPanel1.TabIndex = 2;
-            this.metroPanel1.VerticalScrollbarBarColor = true;
-            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
-            this.metroPanel1.VerticalScrollbarSize = 10;
-            // 
-            // metroLabel1
-            // 
-            this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(16, 15);
-            this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(81, 19);
-            this.metroLabel1.TabIndex = 2;
-            this.metroLabel1.Text = "metroLabel1";
-            // 
-            // metroLabel2
-            // 
-            this.metroLabel2.AutoSize = true;
-            this.metroLabel2.Location = new System.Drawing.Point(16, 46);
-            this.metroLabel2.Name = "metroLabel2";
-            this.metroLabel2.Size = new System.Drawing.Size(83, 19);
-            this.metroLabel2.TabIndex = 3;
-            this.metroLabel2.Text = "metroLabel2";
-            // 
-            // metroLabel3
-            // 
-            this.metroLabel3.AutoSize = true;
-            this.metroLabel3.Location = new System.Drawing.Point(16, 82);
-            this.metroLabel3.Name = "metroLabel3";
-            this.metroLabel3.Size = new System.Drawing.Size(83, 19);
-            this.metroLabel3.TabIndex = 4;
-            this.metroLabel3.Text = "metroLabel3";
-            // 
-            // metroButton1
-            // 
-            this.metroButton1.Location = new System.Drawing.Point(531, 46);
-            this.metroButton1.Name = "metroButton1";
-            this.metroButton1.Size = new System.Drawing.Size(94, 32);
-            this.metroButton1.TabIndex = 5;
-            this.metroButton1.Text = "metroButton1";
-            this.metroButton1.UseSelectable = true;
-            // 
             // Contestar_Cuestionario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -121,9 +59,6 @@
             this.Controls.Add(this.pnContestarCuestionario);
             this.Name = "Contestar_Cuestionario";
             this.Text = "Remover";
-            this.pnContestarCuestionario.ResumeLayout(false);
-            this.metroPanel1.ResumeLayout(false);
-            this.metroPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -132,10 +67,5 @@
 
         private MetroFramework.Controls.MetroPanel pnContestarCuestionario;
         private MetroFramework.Controls.MetroButton btnEnviarCuestionario;
-        private MetroFramework.Controls.MetroPanel metroPanel1;
-        private MetroFramework.Controls.MetroButton metroButton1;
-        private MetroFramework.Controls.MetroLabel metroLabel3;
-        private MetroFramework.Controls.MetroLabel metroLabel2;
-        private MetroFramework.Controls.MetroLabel metroLabel1;
     }
 }
